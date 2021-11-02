5e098e7b6ac5073b806068729e7db849d121b441
Jesús Emerson Pacheco Alanya <jesuspacheco0219@gmail.com>
2021-11-01 22:43:18 -0500

    factory-AppDbnContext

--- Raze.Api/Raze.Api/Users/Persistence/Contexts/AppDbContext.cs
+++ Raze.Api/Raze.Api/Users/Persistence/Contexts/AppDbContext.cs
@@ -13,24 +13,25 @@ namespace Raze.Api.Users.Persistence.Contexts
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
-            //Constrains Users
+            //Inicialitation
             builder.Entity < User>().ToTable("Users");
+            builder.Entity<UserAdvised>().ToTable("Users");
+            builder.Entity<UserAdvisor>().ToTable("Users");
+            //discriminator
+            builder.Entity<User>().HasDiscriminator<string>("user_type")
+                .HasValue<UserAdvisor>("user_advisor")
+                .HasValue<UserAdvised>("user_advised");
+            //Constrains 
             builder.Entity<User>().HasKey(p => p.Id);
             builder.Entity<User>().Property(p => p.Id).IsRequired().ValueGeneratedOnAdd();
-            builder.Entity<User>().Property(p => p.UserName).IsRequired().HasMaxLength(30);
-            builder.Entity<User>().Property(p => p.FirstName).IsRequired();
-            builder.Entity<User>().Property(p => p.LastName).IsRequired();
-            
-            
             //Seed Data Users
-            builder.Entity<User>().HasData(
-                new UserAdvisor{Id = 1,FirstName = "George",LastName = "Boston",UserName = "gerx200",Password = "goku123",Age = 52,Premium =false,TypeUser =EUserType.Advisor,YearsExperience = 20,Rank = 1273,Profession = "Doctor"},
-                new UserAdvisor{Id = 2,FirstName = "Louis",LastName = "Dan",UserName = "luDan",Password = "miau09",Age = 19,Premium =true,TypeUser =EUserType.Advisor,YearsExperience = 4,Rank = 1473,Profession = "Soccer Player"},
-                new UserAdvisor{Id = 3,FirstName = "Drake",LastName = "Bell",UserName = "Drell",Password = "hamburgesa",Age = 23,Premium =false,TypeUser =EUserType.Advisor,YearsExperience = 13,Rank = 273,Profession = "Teacher"}, 
-                new UserAdvised{Id = 4,FirstName = "Loriam",LastName = "KARL",UserName = "kARLO",Password = "dracula25",Age = 22,Premium =false,TypeUser =EUserType.Advised,Mood = 2},
-                new UserAdvised{Id = 5,FirstName = "Dexter",LastName = "Newbe",UserName = "Dex",Password = "Nerito27",Age = 28,Premium =true,TypeUser =EUserType.Advised,Mood = 3}
-              
-                     );
+               builder.Entity<UserAdvised>().HasData(
+                   new UserAdvised{Id = 4,FirstName = "Loriam",LastName = "KARL",UserName = "kARLO",Password = "dracula25",Age = 22,Premium =false,TypeUser =EUserType.Advised,Mood = 2},
+                   new UserAdvised{Id = 5,FirstName = "Dexter",LastName = "Newbe",UserName = "Dex",Password = "Nerito27",Age = 28,Premium =true,TypeUser =EUserType.Advised,Mood = 3}
+            );
+               builder.Entity<UserAdvisor>().HasData(
+                   new UserAdvisor{Id = 3,FirstName = "Drake",LastName = "Bell",UserName = "Drell",Password = "hamburgesa",Age = 23,Premium =false,TypeUser =EUserType.Advisor,YearsExperience = 13,Rank = 273,Profession = "Teacher"}
+               );
         }
     }
 }
