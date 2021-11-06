16e4827229f25ae66f761b3a0f1530f6147ce71e
Jesús Emerson Pacheco Alanya <jesuspacheco0219@gmail.com>
2021-11-06 10:59:53 -0500

    feat add test advised

---
+++ Raze.Api/Raze.API.Tests/UserAdvisedServiceStepsDefinition.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Mime;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Raze.Api;
+using Raze.Api.Resources;
+using Raze.Api.Users.Resources;
+using SpecFlow.Internal.Json;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Assist;
+using Xunit;
+
+namespace Raze.API.Tests
+{
+    [Binding]
+    public class UserAdvisedServiceStepsDefinition
+    {
+        private readonly WebApplicationFactory<Startup> _factory;
+
+        private HttpClient Client { get; set; }
+        private Uri BaseUrl { get; set; }
+        private Task<HttpResponseMessage>Response { get; set; }
+        private UserAdvisedResource UserAdvised { get; set; }
+        public UserAdvisedServiceStepsDefinition(WebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Given(@"the Endpoint https://localhost:(.*)/api/v(.*)/useradviseds is available")]
+        public void GivenTheUseradvisedsEndpointIsAvailable(int port, int version)
+
+        {
+         BaseUrl= new Uri($"https://localhost:{port}/api/v{version}/useradviseds");
+         Client = _factory.CreateClient(new WebApplicationFactoryClientOptions {BaseAddress = BaseUrl});
+        }
+
+        [When(@"A UserAdvised Request is sent")]
+        public void WhenAUserAdvisedRequestIsSent(Table saveUserAdvisedResource)
+        {
+            var resource = saveUserAdvisedResource.CreateSet<SaveUserAdvisedResource>().First();
+            var content = new StringContent(resource.ToJson(), Encoding.UTF8, MediaTypeNames.Application.Json);
+            Response = Client.PostAsync(BaseUrl, content);
+        }
+
+        [Then(@"A Reponse with Status (.*) is received")]
+        public void ThenAReponseWithStatusIsReceived(int expectedStatus)
+        {
+            var expectedStatusCode = ((HttpStatusCode)expectedStatus).ToString();
+            var actualStatusCode = Response.Result.StatusCode.ToString();
+            Assert.Equal(expectedStatusCode,actualStatusCode);
+        }
+    }
+}
---
+++ Raze.Api/Raze.API.Tests/UserAdvisedServiceTest.feature
@@ -0,0 +1,18 @@
+﻿Feature: UserAdvisedServiceTest
+	As a Develover 
+	I want to add new UserAdvised through API
+	So that It can ve available for applications
+
+	Background: 
+		Given the Endpoint https://localhost:5001/api/v1/useradviseds is available
+	
+@useradvised-adding
+Scenario: Add UserAdvised
+	When  A UserAdvised Request is sent
+            | FirstName | LastName | UserName | Password | Premium | Mood |
+          | Yisus     | Graham   | yg25     | goku123  | true    | 3    |	
+          Then A Reponse with Status 200 is received
+          
+          
+
+     
